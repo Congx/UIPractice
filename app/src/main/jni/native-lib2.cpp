b6f7545031ba5d39dcbb127bcb38ea9fe1bac15c
luffy <>
2021-03-24 10:10:12 +0800

    native

--- app/src/main/jni/native-lib2.cpp
+++ app/src/main/jni/native-lib2.cpp
@@ -6,15 +6,12 @@
 //#include <cstdlib>
 #include <string>
 #include <iostream>
-#include "cmaketest/math/include/math.h"
-
-#include "android/bitmap.h"
 
 using namespace std;
 
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_uipractice_ndk_NDKActivity_add(JNIEnv *env, jobject thiz, jint a, jint b) {
-   return math_add(a,b);
+   return 0;
 }
 

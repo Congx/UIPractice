7f1ebfcca43fc30c12fba7530c7e671eda9bc0e8
luffy <>
2021-04-17 11:33:38 +0800

    h264

--- player/ffplayer/src/main/java/com/xc/ffplayer/MainActivity.kt
+++ player/ffplayer/src/main/java/com/xc/ffplayer/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.xc.ffplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnMediaCodec.setOnClickListener {
+            startActivity(Intent(this,MediaCodecActivity::class.java))
+        }
     }
 }

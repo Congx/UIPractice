8c4924c6c871369120b16025f29e02cb561a4dd8
xucong <2762446339@qq.com>
2021-04-17 13:36:51 +0800

    Revert "surface"
    This reverts commit 585203e9

--- player/ffplayer/src/main/java/com/xc/ffplayer/MediaCodecActivity.kt
+++ player/ffplayer/src/main/java/com/xc/ffplayer/MediaCodecActivity.kt
@@ -2,8 +2,6 @@ package com.xc.ffplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.base.utils.toPX
-import kotlinx.android.synthetic.main.activity_media_codec.*
 
 class MediaCodecActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

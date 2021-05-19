11cdf2fa5e392fb0239166dd4a3db5d89fa774c7
luffy <>
2021-05-19 19:09:44 +0800

    opengl start

---
+++ app/src/main/java/com/example/uipractice/opengl/OpenglMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uipractice.opengl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uipractice.R
+
+class OpenglMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opengl_main)
+    }
+}
